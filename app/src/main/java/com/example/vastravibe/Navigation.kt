8598fd4530ee3046382730fde5215a8cf86bb1ee
Arghadeep Chakraborty <argho.chakr@gmail.com>
2024-08-10 19:55:43 -0400

    Landing Screen UI

---
+++ app/src/main/java/com/example/vastravibe/Navigation.kt
@@ -0,0 +1,25 @@
+package com.example.vastravibe
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.vastravibe.screens.LandingScreen
+
+@Composable
+fun Navigation() {
+
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Landing.route) {
+        composable(route = Screen.Landing.route) {
+            LandingScreen(navController = navController)
+        }
+        composable(route = Screen.Dashboard.route) {
+//            LandingScreen(navController = navController)
+        }
+        composable(route = Screen.Product.route) {
+//            LandingScreen(navController = navController)
+        }
+    }
+
+}
